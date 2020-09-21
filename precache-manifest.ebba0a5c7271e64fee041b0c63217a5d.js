a4573fe5f82958ad3b20c67d8df271a577eaa0bf
Germán Martínez <germaga92@gmail.com>
2020-09-21 07:12:48 +0200

    Updates

---
+++ precache-manifest.ebba0a5c7271e64fee041b0c63217a5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd89d04a830e7eb409e7f69f1175069",
+    "url": "/gnosis-safe-calculator/index.html"
+  },
+  {
+    "revision": "fccbc38dc8b5b57685de",
+    "url": "/gnosis-safe-calculator/static/css/main.516d516c.chunk.css"
+  },
+  {
+    "revision": "3034f848b2ed9ed08587",
+    "url": "/gnosis-safe-calculator/static/js/2.4cff0e39.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/gnosis-safe-calculator/static/js/2.4cff0e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fccbc38dc8b5b57685de",
+    "url": "/gnosis-safe-calculator/static/js/main.1dbdb5db.chunk.js"
+  },
+  {
+    "revision": "fb46a3cacb4e953b0b20",
+    "url": "/gnosis-safe-calculator/static/js/runtime-main.4ff601b5.js"
+  },
+  {
+    "revision": "47c7a29f39e976cbad1da973c9f6fc92",
+    "url": "/gnosis-safe-calculator/static/media/Averta-ExtraBold.47c7a29f.woff2"
+  },
+  {
+    "revision": "30559e37a0ef8a886f814f588bfea868",
+    "url": "/gnosis-safe-calculator/static/media/Averta-normal.30559e37.woff2"
+  }
+]);
